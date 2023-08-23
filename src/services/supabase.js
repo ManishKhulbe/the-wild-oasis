75fdbbc788e3a0b8a26ed7efcb0b395a6b2c1588
manishkhulbe <manishkhulbe123@gmail.com>
2023-08-23 18:21:23 +0530

    supabase complete , react query start

---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://dkmeepmvsjugthzdccrt.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRrbWVlcG12c2p1Z3RoemRjY3J0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTI3MDYzMzIsImV4cCI6MjAwODI4MjMzMn0.BHiNda - pvPGuIwKsAe_S7BnMwXKImjXVXYNyVjImlQ8";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
